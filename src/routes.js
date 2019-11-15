abe3ce7c8fa96fa4f862ab97a00c0fb2beb6872b
milaendo <milal.alim@gmail.com>
2019-11-15 15:39:15 -0800

    add classroom route

--- src/routes.js
+++ src/routes.js
@@ -23,6 +23,8 @@ const ButtonGroups = React.lazy(() => import('./views/Buttons/ButtonGroups'));
 const Buttons = React.lazy(() => import('./views/Buttons/Buttons'));
 const Charts = React.lazy(() => import('./views/Charts'));
 const Dashboard = React.lazy(() => import('./views/Dashboard'));
+const Classroom = React.lazy(() => import('./views/Classroom'));
+const RequestFlights = React.lazy(() => import('./views/RequestFlights'));
 const CoreUIIcons = React.lazy(() => import('./views/Icons/CoreUIIcons'));
 const Flags = React.lazy(() => import('./views/Icons/Flags'));
 const FontAwesome = React.lazy(() => import('./views/Icons/FontAwesome'));
@@ -40,6 +42,8 @@ const User = React.lazy(() => import('./views/Users/User'));
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/dashboard', name: 'Dashboard', component: Dashboard },
+  { path: '/classroom', name: 'Classroom', component: Classroom },
+  { path: '/request-flights', name: 'requestFlights', component: RequestFlights },
   { path: '/theme', exact: true, name: 'Theme', component: Colors },
   { path: '/theme/colors', name: 'Colors', component: Colors },
   { path: '/theme/typography', name: 'Typography', component: Typography },
