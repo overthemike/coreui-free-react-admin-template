2a524c2e42f2fe6542ad0594283e110bde9353d2
milaendo <milal.alim@gmail.com>
2019-11-22 11:33:53 -0800

    headers for axios

--- src/redux/ducks/forms/index.js
+++ src/redux/ducks/forms/index.js
@@ -1,16 +1,16 @@
 // imports
-import axios from "axios"
-import { useSelector, useDispatch } from "react-redux"
+import axios from "axios";
+import { useSelector, useDispatch } from "react-redux";
 
 // action type definitions
-const REQUEST_CARD = "auth/REQUEST_CARD"
+const REQUEST_CARD = "auth/REQUEST_CARD";
 
 // initial state
 const initialState = {
   inquiry: "",
   wallet_updated: "",
   notes: ""
-}
+};
 
 // reducer (MUST BE DEFAULT EXPORT)
 export default (state = initialState, action) => {
@@ -21,36 +21,48 @@ export default (state = initialState, action) => {
         inquiry: action.payload.inquiry,
         wallet_updated: action.payload.wallet,
         notes: action.payload.notes
-      }
+      };
     default:
-      return state
+      return state;
   }
-}
+};
 
 // action creators
-function submitForm (inquiry, wallet_updated, notes, dispatch) {
+function submitForm(inquiry, wallet_updated, notes, dispatch) {
   return new Promise((resolve, reject) => {
     axios
-      .post("/customer-requests/card-inquiry/", { inquiry, wallet_updated, notes, dispatch }, {headers: {token: window.localStorage.getItem('token')}})
+      .post(
+        "/customer-requests/card-inquiry/",
+        {
+          inquiry,
+          wallet_updated,
+          notes,
+          dispatch
+        },
+        { Authorization: "Token " + window.localStorage.getItem("token") }
+      )
       .then(resp => {
         dispatch({
           type: REQUEST_CARD,
           payload: { inquiry, wallet_updated, notes, dispatch }
-        })
-        resolve()
+        });
+        resolve();
       })
       .catch(e => {
-        reject(e)
-      })
-  })
+        reject(e);
+      });
+  });
 }
 // custom hooks
 export function useForms() {
-  const dispatch = useDispatch()
-  const inquiry = useSelector(appState => appState.formState.inquiry)
-  const wallet_updated = useSelector(appState => appState.formState.wallet_updated)
-  const notes = useSelector(appState => appState.formState.notes)
-  const requestCard = (inquiry, wallet_updated, notes) => submitForm(inquiry, wallet_updated, notes, dispatch)
+  const dispatch = useDispatch();
+  const inquiry = useSelector(appState => appState.formState.inquiry);
+  const wallet_updated = useSelector(
+    appState => appState.formState.wallet_updated
+  );
+  const notes = useSelector(appState => appState.formState.notes);
+  const requestCard = (inquiry, wallet_updated, notes) =>
+    submitForm(inquiry, wallet_updated, notes, dispatch);
 
-  return { inquiry, wallet_updated, notes, requestCard }
+  return { inquiry, wallet_updated, notes, requestCard };
 }
