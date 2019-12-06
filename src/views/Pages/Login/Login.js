ef2a27242f1bad74b050200b3bec4eb2d09f882f
milaendo <milal.alim@gmail.com>
2019-12-06 12:58:49 -0800

    new login design

--- src/views/Pages/Login/Login.js
+++ src/views/Pages/Login/Login.js
@@ -1,19 +1,5 @@
 import React, { useState } from "react";
-import pic from "../../../assets/img/brand/black.png";
-import {
-  Button,
-  Card,
-  CardBody,
-  CardGroup,
-  Col,
-  Container,
-  Form,
-  Input,
-  InputGroup,
-  InputGroupAddon,
-  InputGroupText,
-  Row
-} from "reactstrap";
+import { Button, Form, Input } from "reactstrap";
 import { useAuth } from "../../../hooks";
 
 function Login(props) {
@@ -32,78 +18,43 @@ function Login(props) {
   }
 
   return (
-    <div className="app flex-row align-items-center">
-      <Container>
-        <Row className="justify-content-center">
-          <Col md="8">
-            <CardGroup>
-              <Card className="p-4">
-                <CardBody>
-                  <Form onSubmit={handleSubmit}>
-                    <h1>Login</h1>
-                    <p className="text-muted">Sign In to your account</p>
-                    <InputGroup className="mb-3">
-                      <InputGroupAddon addonType="prepend">
-                        <InputGroupText>
-                          <i className="icon-user" />
-                        </InputGroupText>
-                      </InputGroupAddon>
-                      <Input
-                        type="text"
-                        placeholder="Username"
-                        autoComplete="username"
-                        value={username}
-                        onChange={e => setUsername(e.target.value)}
-                      />
-                    </InputGroup>
-                    <InputGroup className="mb-4">
-                      <InputGroupAddon addonType="prepend">
-                        <InputGroupText>
-                          <i className="icon-lock" />
-                        </InputGroupText>
-                      </InputGroupAddon>
-                      <Input
-                        type="password"
-                        placeholder="Password"
-                        autoComplete="current-password"
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}
-                      />
-                    </InputGroup>
-                    <Row>
-                      <Col xs="6">
-                        <Button type="submit" color="primary" className="px-4">
-                          Login
-                        </Button>
-                      </Col>
-                      <Col xs="6" className="text-right">
-                        <Button color="link" className="px-0">
-                          Forgot password?
-                        </Button>
-                      </Col>
-                    </Row>
-                  </Form>
-                </CardBody>
-              </Card>
-              <Card
-                className="text-white bg-primary py-5 d-md-down-none"
-                style={{ width: "44%" }}
-              >
-                <CardBody className="text-center">
-                  <div>
-                    <img
-                      src={pic}
-                      className="img-avatar"
-                      alt="admin@bootstrapmaster.com"
-                    />
-                  </div>
-                </CardBody>
-              </Card>
-            </CardGroup>
-          </Col>
-        </Row>
-      </Container>
-    </div>
+    <>
+      <div class="login-body"></div>
+      <div class="grad"></div>
+      <div class="header">
+        <div>
+          Travel<span className="text-primary">Wealth</span>
+        </div>
+      </div>
+      <br />
+      <div class="login">
+        <Form onSubmit={handleSubmit}>
+          <Input
+            className="mb-2"
+            type="text"
+            placeholder="Username"
+            autoComplete="username"
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+          />
+          <Input
+            className="mb-2"
+            type="password"
+            placeholder="Password"
+            autoComplete="current-password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+          <Button
+            type="submit"
+            color="primary"
+            className="btn btn-primary btn-lg btn-block"
+          >
+            Login
+          </Button>
+        </Form>
+      </div>
+    </>
   );
 }
 
