705a5e9e3a925002f33a6861d84d9a2b7144691f
milaendo <milal.alim@gmail.com>
2019-12-17 09:54:38 -0800

    edit/delete cards

--- src/redux/ducks/cards/index.js
+++ src/redux/ducks/cards/index.js
@@ -3,6 +3,8 @@ import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 // action type definitions
 const GET_ADMIN_CARDS = "wallet/GET_ADMIN_CARDS";
+const EDIT_ADMIN_CARDS = "wallet/EDIT_ADMIN_CARDS";
+const DELETE_ADMIN_CARDS = "wallet/DELETE_ADMIN_CARDS";
 
 // initial state
 const initialState = {
@@ -16,6 +18,15 @@ export default (state = initialState, action) => {
       return {
         ...state,
         cards: action.payload
+      };
+    case EDIT_ADMIN_CARDS:
+      return {
+        ...state,
+        cards: action.payload
+      };
+    case DELETE_ADMIN_CARDS:
+      return {
+        ...state
       };
     default:
       return state;
@@ -39,13 +50,125 @@ function getRecCards() {
       });
   };
 }
+function editAdminCards(
+  id,
+  name,
+  use_for,
+  annual_fee,
+  annual_notes,
+  app_link,
+  approval_524,
+  bonus_deadline,
+  counts_524,
+  features,
+  first_year_free,
+  free_intl,
+  image,
+  recon_line,
+  rewards,
+  type
+) {
+  return dispatch => {
+    const accessToken = window.localStorage.getItem("token");
+    axios.defaults.headers.common["Authorization"] = "Token " + accessToken;
+    axios({
+      method: "put",
+      url: "/cards/" + id,
+      data: {
+        id,
+        name,
+        use_for,
+        annual_fee,
+        annual_notes,
+        app_link,
+        approval_524,
+        bonus_deadline,
+        counts_524,
+        features,
+        first_year_free,
+        free_intl,
+        image,
+        recon_line,
+        rewards,
+        type
+      },
+      headers: { Authorization: "Token " + accessToken }
+    })
+      .then(resp => {
+        dispatch({
+          type: EDIT_ADMIN_CARDS,
+          payload: resp.data
+        });
+        window.location.reload();
+      })
+      .catch(e => {
+        console.error("ERROR", e);
+      });
+  };
+}
+function deleteAdminCards(id) {
+  return dispatch => {
+    const accessToken = window.localStorage.getItem("token");
+    axios.defaults.headers.common["Authorization"] = "Token " + accessToken;
+    axios
+      .delete("/cards/" + id)
+      .then(resp => {
+        dispatch({
+          type: DELETE_ADMIN_CARDS
+        });
+        window.location.reload();
+      })
+      .catch(e => {
+        console.error("ERROR", e);
+      });
+  };
+}
 
 export function useAdminCards() {
   const dispatch = useDispatch();
   const adminCards = useSelector(appState => appState.myAdminCardsState.cards);
+  const deleteCards = id => dispatch(deleteAdminCards(id));
+  const editCards = (
+    id,
+    name,
+    use_for,
+    annual_fee,
+    annual_notes,
+    app_link,
+    approval_524,
+    bonus_deadline,
+    counts_524,
+    features,
+    first_year_free,
+    free_intl,
+    image,
+    recon_line,
+    rewards,
+    type
+  ) =>
+    dispatch(
+      editAdminCards(
+        id,
+        name,
+        use_for,
+        annual_fee,
+        annual_notes,
+        app_link,
+        approval_524,
+        bonus_deadline,
+        counts_524,
+        features,
+        first_year_free,
+        free_intl,
+        image,
+        recon_line,
+        rewards,
+        type
+      )
+    );
 
   useEffect(() => {
     dispatch(getRecCards());
   }, [dispatch]);
-  return { adminCards };
+  return { adminCards, deleteCards, editCards };
 }
--- src/views/Cards/Cards.js
+++ src/views/Cards/Cards.js
@@ -1,50 +1,69 @@
-import React, { useState, useMemo, useCallback } from "react";
+import React, { useState, useMemo } from "react";
 import DataTable from "react-data-table-component";
 import { useAdminCards } from "../../hooks";
-import { Button, Modal, ModalBody, ModalHeader, Input } from "reactstrap";
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalHeader,
+  Input,
+  Row,
+  Col,
+  Form,
+  Label
+} from "reactstrap";
 
 function Cards(props) {
   const { adminCards } = useAdminCards();
   const [pending, setPending] = React.useState(true);
   const [singleCard, setSingleCard] = useState("");
-  // const handleAction = value => setSingleCard(value)
-  // const updateState = useCallback(state => handleRowClick(state));
   const [modal2, setModal2] = useState(false);
   const [modal, setModal] = useState(false);
   const data2 = adminCards;
   const [filterText, setFilterText] = React.useState("");
+  const { deleteCards, editCards } = useAdminCards();
+
   const columns2 = useMemo(() => [
     {
       name: "Cards",
       selector: "image",
       sortable: false,
+      maxWidth: "50px",
       cell: row => <img className="tableCards" alt={row.name} src={row.image} />
     },
     {
       name: "Name",
       selector: "name",
-      sortable: true
+      maxWidth: "250px",
+      sortable: true,
+      wrap: true
     },
     {
-      name: "Account Holder",
-      selector: "bonus_amount",
+      name: "Use For",
+      selector: "use_for",
+      sortable: true,
+      maxWidth: "200px",
+      wrap: true
+    },
+    {
+      name: "Features",
+      selector: "features",
+      maxWidth: "200px",
       sortable: true
     },
     {
       name: "Fee",
       selector: "annual_fee",
+      maxWidth: "50px",
       sortable: true
     },
     {
-      name: "Use For",
-      selector: "use_for",
+      name: "Annual Notes",
+      selector: "annual_notes",
+      maxWidth: "250px",
       sortable: true
     },
-    {
-      name: "Features",
-      selector: "features",
-      sortable: true
-    },
+
     {
       cell: row => (
         <ActionComponent row={row} onClick={handleRowClick}>
@@ -53,25 +72,34 @@ function Cards(props) {
       ),
       ignoreRowClick: false,
       allowOverflow: true,
-      button: true
+      button: true,
+      right: true
     }
   ]);
-
+  const SampleExpandedComponent = ({ data }) => (
+    <div className="mt-3">
+      <p className="text-primary">Annual Notes : {data.annual_notes}</p>
+    </div>
+  );
   function handleRowClick(row) {
-    // console.log("CLICK", row);
     setSingleCard(row);
     toggle2(row);
   }
 
-  function handleDelete(row) {
-    console.log("delete", row);
+  function openDeleteModal(row) {
     setSingleCard(row);
-
     toggle(row);
   }
-  function deleteCard(id) {
-    console.log("DELETE");
+
+  async function handleDeleteCard(id) {
+    try {
+      await deleteCards(id);
+      toggle();
+    } catch (e) {
+      console.log(e);
+    }
   }
+
   const CustomLoader = () => (
     <div>
       <i className="fas fa-circle-notch fa-spin fa-5x text-secondary"></i>
@@ -80,7 +108,7 @@ function Cards(props) {
 
   const ActionComponent = ({ row, onClick }) => {
     const clickHandler = () => handleRowClick(row);
-    const deleteHandler = () => handleDelete(row);
+    const deleteHandler = () => openDeleteModal(row);
 
     return (
       <>
@@ -98,7 +126,231 @@ function Cards(props) {
       </>
     );
   };
+  const DeleteButton = ({ singleCard, onClick }) => {
+    const buttonHandler = () => handleDeleteCard(singleCard);
 
+    return (
+      <Button
+        className="btn btn-outline-secondary text-light align-right"
+        onClick={buttonHandler}
+      >
+        delete
+      </Button>
+    );
+  };
+  const EditModal = ({ singleCard, isOpen, onClick }) => {
+    const editHandler = () => updateCard(singleCard.id);
+
+    const id = singleCard.id;
+    const [name, setName] = useState(singleCard.name);
+    const [useFor, setUseFor] = useState(singleCard.use_for);
+    const [annualFee, setAnnualFee] = useState(singleCard.annual_fee);
+    const [annualNotes, setAnnualNotes] = useState(singleCard.annual_notes);
+    const [appLink, setAppLink] = useState(singleCard.app_link);
+    const [approval524, setApproval524] = useState(
+      singleCard.approval_over_524
+    );
+    const [bonusDeadline, setBonusDeadline] = useState(
+      singleCard.bonus_deadline
+    );
+    const [counts524, setCounts524] = useState(singleCard.counts_524);
+    const [features, setFeatures] = useState(singleCard.features);
+    const [firstYearFree, setFirstYearFree] = useState(
+      singleCard.first_year_free
+    );
+    const [freeIntl, setFreeIntl] = useState(singleCard.free_intl);
+    const [image, setImage] = useState(singleCard.image);
+    const [reconLine, setReconLine] = useState(singleCard.recon_line);
+    const [rewards, setRewards] = useState(singleCard.rewards);
+    const [type, setType] = useState(singleCard.type);
+    async function updateCard() {
+      try {
+        await editCards(
+          id,
+          name,
+          useFor,
+          annualFee,
+          annualNotes,
+          appLink,
+          approval524,
+          bonusDeadline,
+          counts524,
+          features,
+          firstYearFree,
+          freeIntl,
+          image,
+          reconLine,
+          rewards,
+          type
+        );
+        toggle2();
+      } catch (e) {
+        console.log("HERE?", e);
+      }
+    }
+    return (
+      <Modal isOpen={modal2} toggle={toggle2} className={props.className}>
+        <ModalHeader toggle={toggle2} className="d-flex">
+          <i className="fas fa-plus"></i> Add a new card
+        </ModalHeader>
+        <ModalBody>
+          <Form onSubmit={editHandler}>
+            <Row>
+              <Col xs="12" md="6">
+                <Label htmlFor="depart">Name</Label>
+                <Input
+                  type="text"
+                  id="depart"
+                  value={name}
+                  label="name"
+                  onChange={e => setName(e.target.value)}
+                />
+              </Col>
+              <Col xs="12" md="6">
+                <Label htmlFor="depart">Use For</Label>
+                <Input
+                  type="text"
+                  id="depart"
+                  value={useFor}
+                  onChange={e => setUseFor(e.target.value)}
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Label htmlFor="depart">Annual Fee</Label>
+                <Input
+                  type="text"
+                  id="depart"
+                  value={annualFee}
+                  onChange={e => setAnnualFee(e.target.value)}
+                />
+              </Col>
+              <Col>
+                <Label htmlFor="depart">Type</Label>
+                <Input
+                  type="text"
+                  id="depart"
+                  value={type}
+                  onChange={e => setType(e.target.value)}
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Label htmlFor="depart">App Link</Label>
+                <Input
+                  type="text"
+                  id="depart"
+                  value={appLink}
+                  onChange={e => setAppLink(e.target.value)}
+                />
+              </Col>
+              <Col>
+                <Label htmlFor="depart">Approval 524</Label>
+                <Input
+                  type="text"
+                  id="depart"
+                  value={approval524}
+                  onChange={e => setApproval524(e.target.value)}
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Label htmlFor="depart">Bonus Deadline</Label>
+                <Input
+                  type="text"
+                  id="depart"
+                  value={bonusDeadline}
+                  onChange={e => setBonusDeadline(e.target.value)}
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Label htmlFor="depart">Counts 524</Label>
+                <Input
+                  type="text"
+                  id="depart"
+                  value={counts524}
+                  onChange={e => setCounts524(e.target.value)}
+                />
+              </Col>
+              <Col>
+                <Label htmlFor="depart">features</Label>
+                <Input
+                  type="textarea"
+                  id="depart"
+                  value={features}
+                  onChange={e => setFeatures(e.target.value)}
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Label htmlFor="depart">First Year Free</Label>
+                <Input
+                  type="text"
+                  id="depart"
+                  value={firstYearFree}
+                  onChange={e => setFirstYearFree(e.target.value)}
+                />
+              </Col>
+              <Col>
+                <Label htmlFor="depart">Free Intl</Label>
+                <Input
+                  type="text"
+                  id="depart"
+                  value={freeIntl}
+                  onChange={e => setFreeIntl(e.target.value)}
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Label htmlFor="depart">Image</Label>
+                <Input
+                  type="textarea"
+                  id="depart"
+                  value={image}
+                  onChange={e => setImage(e.target.value)}
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Label htmlFor="depart">Recon Line</Label>
+                <Input
+                  type="text"
+                  id="depart"
+                  value={reconLine}
+                  onChange={e => setReconLine(e.target.value)}
+                />
+              </Col>
+              <Col>
+                <Label htmlFor="depart">Rewards</Label>
+                <Input
+                  type="text"
+                  id="depart"
+                  value={rewards}
+                  onChange={e => setRewards(e.target.value)}
+                />
+              </Col>
+            </Row>
+            <Label htmlFor="depart">Annual Notes</Label>
+            <Input
+              type="textarea"
+              id="depart"
+              value={annualNotes}
+              onChange={e => setAnnualNotes(e.target.value)}
+            />
+            <Button className="float-right mt-3">Save</Button>
+          </Form>
+        </ModalBody>
+      </Modal>
+    );
+  };
   React.useEffect(() => {
     const timeout = setTimeout(() => {
       setPending(false);
@@ -112,76 +364,72 @@ function Cards(props) {
   function toggle(row) {
     setModal(!modal);
   }
-  const FilterComponent = ({ filterText, onFilter, onClear }) => (
-    <>
-      <Input
-        id="search"
-        type="text"
-        placeholder="Filter By Name"
-        value={filterText}
-        onChange={onFilter}
-      />
-      <Button
-        className="btn btn-outline-secondary text-light"
-        onClick={onClear}
-      >
-        X
-      </Button>
-    </>
-  );
+  // const FilterComponent = ({ filterText, onFilter, onClear }) => (
+  //   <>
+  //     <Input
+  //       id="search"
+  //       type="text"
+  //       placeholder="Filter By Name"
+  //       value={filterText}
+  //       onChange={onFilter}
+  //     />
+  //     <Button
+  //       className="btn btn-outline-secondary text-light"
+  //       onClick={onClear}
+  //     >
+  //       X
+  //     </Button>
+  //   </>
+  // );
 
-  const filteredItems = data2.filter(
-    item => item.name && item.name.includes(filterText)
-  );
+  // const filteredItems = data2.filter(
+  //   item => item.name && item.name.includes(filterText)
+  // );
 
-  const subHeaderComponentMemo = React.useMemo(() => {
-    const handleClear = () => {
-      if (filterText) {
-        setFilterText("");
-      }
-    };
+  // const subHeaderComponentMemo = React.useMemo(() => {
+  //   const handleClear = () => {
+  //     if (filterText) {
+  //       setFilterText("");
+  //     }
+  //   };
 
-    return (
-      <FilterComponent
-        onFilter={e => setFilterText(e.target.value)}
-        onClear={handleClear}
-        filterText={filterText}
-      />
-    );
-  }, [filterText]);
+  //   return (
+  //     <FilterComponent
+  //       onFilter={e => setFilterText(e.target.value)}
+  //       onClear={handleClear}
+  //       filterText={filterText}
+  //     />
+  //   );
+  // }, [filterText]);
   return (
     <>
       <DataTable
-        data={filteredItems}
+        data={data2}
         pagination
+        expandableRows
         subHeader
-        subHeaderComponent={subHeaderComponentMemo}
+        // subHeaderComponent={subHeaderComponentMemo}
         title="Card Listing"
         columns={columns2}
         progressPending={pending}
         progressComponent={<CustomLoader />}
-        // onRowClicked={updateState}
         highlightOnHover
         persistTableHead
+        expandableRowsComponent={<SampleExpandedComponent />}
       />
-      <Modal isOpen={modal2} toggle={toggle2} className={props.className}>
-        <ModalHeader toggle={toggle2} className="d-flex">
-          <i className="fas fa-plus"></i> Add a new card
-        </ModalHeader>
-        <ModalBody>{singleCard.name}</ModalBody>
-      </Modal>
+      <EditModal isOpen={modal2} singleCard={singleCard} onClick={editCards} />
       <Modal isOpen={modal} toggle={toggle} className={props.className}>
         <ModalHeader toggle={toggle} className="d-flex">
           <i className="fas fa-plus"></i> Delete Card
         </ModalHeader>
         <ModalBody>
-          {singleCard.name}
-          <Button
-            className="btn btn-outline-secondary text-light"
-            onClick={deleteCard(singleCard.id)}
-          >
-            delete
-          </Button>
+          are you sure you want to delete {singleCard.name} ?
+          <Row className="float-right mt-4 mr-2">
+            <DeleteButton
+              onClick={handleDeleteCard}
+              singleCard={singleCard.id}
+            />
+          </Row>
         </ModalBody>
       </Modal>
     </>
